ba9b0f92647eae561179f93dc0c419ab1429df71
KAMWINE JONAN <kamwinejonan55@gmail.com>
2025-03-27 19:38:06 +0300

    my commit

--- api/views.py
+++ api/views.py
@@ -128,3 +128,4 @@ class CreateUserView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
