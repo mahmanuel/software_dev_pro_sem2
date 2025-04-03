189f5a245f3e0563d45b83f4c30ce1574c897919
muganzibaker <bmuganzi0@gmail.com>
2025-04-03 10:23:20 +0300

    I added api.js

---
+++ frontend/src/api.js
@@ -0,0 +1,19 @@
+// frontend/src/api.js
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:5000', // Update with the real backend URL
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
