8be8a099dc657376292f8cb3bc4c5c47b757a2d7
KAMWINE JONAN <kamwinejonan55@gmail.com>
2025-04-03 15:54:49 +0300

    deletion similar

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets, generics
 from django.contrib.auth import authenticate
 from rest_framework import status, generics, permissions
-from rest_framework.response import Response
+
 from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from django.contrib.auth import get_user_model
