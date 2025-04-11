cc5f5cc3e8ffd2772f2e199f57014c21437d0515
Komukamatracy <tracykomukama9@gmail.com>
2025-04-11 15:28:15 +0300

    LecturerDashboard.jsx

--- frontend-work/main.jsx
+++
@@ -1,9 +0,0 @@
-import { strictmode } from 'react'
-import { createRoot } from 'react-dome/client'
-import './index.css'
-import App from './App.jsx'
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
