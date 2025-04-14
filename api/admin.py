29503666563fb2352ad5fdb2a9db3d082af707e5
MARIONnabaggala <nabaggalamarion9@gmail.com>
2025-04-14 20:42:08 +0300

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import User, Profile, Issue, Assignment, AuditLog,Notification
 
- Register your models here.
+# Register your models here.
 admin.site.register(User)
 admin.site.register(Profile)
 admin.site.register(Issue)
