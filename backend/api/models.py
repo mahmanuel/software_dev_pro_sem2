ebcd6c5139fd3d89443799852790a7f1a054cace
KAMWINE JONAN <kamwinejonan55@gmail.com>
2025-04-14 18:48:28 +0300

    cmt

--- backend/api/models.py
+++ backend/api/models.py
@@ -150,4 +150,4 @@ class Message(models.Model):
     )
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    is_deleted = models.BooleanField
+    is_deleted = models.BooleanField(default=False)
