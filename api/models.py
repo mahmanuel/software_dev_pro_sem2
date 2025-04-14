b772c161f6616962d5a3355faa75184672055296
MARIONnabaggala <nabaggalamarion9@gmail.com>
2025-04-14 20:43:54 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -12,7 +12,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
 
 
 # Custom User Model
